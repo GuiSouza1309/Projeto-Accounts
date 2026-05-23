6f0c0950352e9f7b63e2187d8b1f64ad8c50fa8e
Guilherme Souza <guidafonseca2006@gmail.com>
2026-05-23 11:18:18 -0300

    feat: cria menu de opções e corrige compatibilidade do inquirer e chalk

--- index.js
+++ index.js
@@ -2,4 +2,57 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const fs = require('fs');
 
-console.log("Iniciamos o Accounts")
+operation();
+
+function operation() {
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+        },
+    ]).then((answer) => {
+        const action = answer['action'];
+
+        if (action === 'Criar Conta') {
+            createAccount();
+        }
+    }).catch((err) => console.log(err));
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir: '));
+    buildAccount();
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:'
+        },
+    ]).then((answer) => {
+        const accountName = answer['accountName'];
+        console.info(accountName);
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts');
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'));
+            buildAccount();
+            return;
+        }
+        try {
+            fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}');
+            console.log(chalk.green('Parabéns, sua conta foi criada!'));
+        } catch (err) {
+            console.log(chalk.bgRed.black('Erro ao criar o arquivo da conta:'), err);
+        }
+        operation(); 
+    })
+    .catch((err) => console.log(err))
+}
