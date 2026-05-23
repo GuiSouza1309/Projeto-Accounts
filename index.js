b2d942f64c43dc9d79c5fbb0bdb363451f305605
Guilherme Souza <guidafonseca2006@gmail.com>
2026-05-23 12:02:13 -0300

    feat: implementa funcionalidade de saque e atualização de saldo

--- index.js
+++ index.js
@@ -133,3 +133,43 @@ function getAccountBalance() {
         operation();
     }).catch((err) => console.log(err));
 }
+
+function withdraw() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Qual o nome da sua conta?'
+        },
+    ]).then((answer) => {
+        const accountName = answer['accountName'];
+        if (!checkAccount(accountName)) {
+            return withdraw();
+        }
+
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'Quanto você deseja sacar?'
+            }
+        ]).then((answer) => {
+            const amount = parseFloat(answer['amount']);
+            subtractAmount(accountName, amount);
+        }).catch((err) => console.log(err));
+    }).catch((err) => console.log(err));
+}
+
+function subtractAmount(accountName, amount) {
+    const accountData = getAccount(accountName);
+    if (!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+        return withdraw();
+    }
+    if (accountData.balance < amount) {
+        console.log(chalk.bgRed.black('Valor indisponível!'));
+        return withdraw();
+    }
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+    fs.writeFileSync(`accounts/${accountName}.json`, JSON.stringify(accountData), (err) => console.log(err));
+    console.log(chalk.green(`Foi sacado o valor de R$${amount} da sua conta!`));
+    operation();
+}
